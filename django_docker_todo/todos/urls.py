c28fd3a2a16c23179edfdd83bdb991de25a539e3
alfik <alfik.m@webandcrafts.com>
2024-03-27 16:21:28 +0530

    initial commit

---
+++ django_docker_todo/todos/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import TodoList,TodoDetail
+urlpatterns = [
+    path('todos/', TodoList.as_view(), name='todo-list'),
+    path('todos/<int:pk>/', TodoDetail.as_view(), name='todo-detail'),
+]
